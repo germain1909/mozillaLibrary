3109aa0968087253ac92e9abca7a0e7668578637
Germain Burchfield <germainburchfield@Germains-iMac.attlocal.net>
2020-07-23 08:10:55 -0400

    add tabular inline class book inside of the author class

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,17 +3,25 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance
 
+
+#Inline classes 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BookInline(admin.TabularInline):
+    model = Book
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')] #purely to change on screen layout
-
+    inlines=[BookInline]
+    
 #admin.site.register(Book)
 admin.site.register(Author,AuthorAdmin)
 admin.site.register(Genre)
 #admin.site.register(BookInstance)
 
-class BooksInstanceInline(admin.TabularInline):
-    model = BookInstance
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
