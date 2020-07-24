6b1c9f01ae53b45799ba7ff23d0b2b2e22faca2f
Germain Burchfield <germainburchfield@Germains-iMac.attlocal.net>
2020-07-24 07:58:52 -0400

    Update site to have list view and detail view for books usingclass based generic view

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,8 +3,21 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
 #path(url pattern,view function, name to be used to reverse url)
 #reverse url means  to dynamically create a URL that  points to the
 # resource that the mapper is designed to handle.
 #Example <a href="{% url 'index' %}">Home</a>.
+
+#For Django class-based views we access an appropriate view function by calling the class method as_view()
+
+#The syntax is very simple: angle brackets define the part of the 
+# URL to be captured, enclosing the name of the variable that the 
+# view can use to access the captured data. 
+# For example, <something> , will capture the marked pattern 
+# and pass the value to the view as a variable "something". 
+# You can optionally precede the variable name with a 
+# converter specification that defines the type of 
+# data (int, str, slug, uuid, path).
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 
@@ -29,3 +30,22 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     #Render method render(request object,html template with placeholders for data, context which is a python dictionary with the data for the template)
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'book_list.html'  # Specify your own template name/location
+
+    
+#The generic view will query the database to get all records for the 
+# specified model (Book) then render a template 
+# located at /locallibrary/catalog/templates/catalog/book_list.html 
+# (which we will create below). Within the template you can access the
+# list of books with the template variable name object_list OR book_list (
+# i.e. generically "the_model_name_list").
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'  # Specify your own template name/location
+    
+#aboce looks for template book_detail because takes the model name and adds _detail
