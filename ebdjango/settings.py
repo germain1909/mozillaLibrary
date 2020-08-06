bb4e1c548ce6d56e614cccc9d4726406d49998c9
Germain Burchfield <germainburchfield@Germains-iMac.attlocal.net>
2020-08-05 22:29:21 -0400

    try with new database

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -80,9 +80,9 @@ if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'NAME': 'ebdb',
+            'USER': 'root',
+            'PASSWORD': 'rootroot',
             'HOST': os.environ['RDS_HOSTNAME'],
             'PORT': os.environ['RDS_PORT'],
         }
